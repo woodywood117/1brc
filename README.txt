a912fcc8c05dfde37259cf50c1ab812dcbb9f50e
james.wood <jwooddev@gmail.com>
2024-01-09 00:02:51 -0600

    Update gitignore; add test for profiling

---
+++ README.txt
@@ -0,0 +1,24 @@
+Execution #1:
+Executed in  142.39 secs    fish           external
+   usr time  138.28 secs    0.18 millis  138.28 secs
+   sys time    5.10 secs    1.62 millis    5.10 secs
+
+Execution #2:
+Executed in  142.25 secs    fish           external
+   usr time  138.64 secs  195.00 micros  138.64 secs
+   sys time    5.03 secs  821.00 micros    5.03 secs
+
+Execution #3:
+Executed in  142.99 secs    fish           external
+   usr time  139.33 secs    0.18 millis  139.33 secs
+   sys time    5.00 secs    1.26 millis    5.00 secs
+
+Execution #4:
+Executed in  142.13 secs    fish           external
+   usr time  138.53 secs    0.18 millis  138.53 secs
+   sys time    4.99 secs    1.42 millis    4.99 secs
+
+Execution #5:
+Executed in  141.75 secs    fish           external
+   usr time  138.02 secs  179.00 micros  138.02 secs
+   sys time    5.14 secs  579.00 micros    5.14 secs
--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,14 @@
 package cmd
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -19,5 +26,73 @@ func Execute() {
 }
 
 func run(cmd *cobra.Command, args []string) {
-	// TODO: Put your code here.
+	type Measurement struct {
+		Min   float64
+		Max   float64
+		Sum   float64
+		Count int
+	}
+	var measurements = make(map[string]*Measurement)
+
+	file, err := os.Open("measurements_1M.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	buffer := bufio.NewReader(file)
+
+	// Read file line by line and calculate
+	var line string
+	for {
+		line, err = buffer.ReadString('\n')
+		if err != nil {
+			break
+		}
+		line = line[:len(line)-1]
+
+		parts := strings.Split(line, ";")
+		if len(parts) != 2 {
+			err = errors.New("invalid line")
+			break
+		}
+
+		value, err := strconv.ParseFloat(parts[1], 64)
+		if err != nil {
+			err = errors.New("invalid line")
+			break
+		}
+
+		measurement, ok := measurements[parts[0]]
+		if !ok {
+			measurement = &Measurement{
+				Min:   100,
+				Max:   0,
+				Sum:   0,
+				Count: 0,
+			}
+			measurements[parts[0]] = measurement
+		}
+		measurement.Min = min(measurement.Min, value)
+		measurement.Max = max(measurement.Max, value)
+		measurement.Sum += value
+		measurement.Count++
+	}
+	if err != nil && !errors.Is(err, io.EOF) {
+		panic(err)
+	}
+
+	// Sort keys alphabetically
+	var keys []string
+	for key := range measurements {
+		keys = append(keys, key)
+	}
+	slices.Sort(keys)
+
+	// Print results
+	for _, key := range keys {
+		measurement := measurements[key]
+		avg := measurement.Sum / float64(measurement.Count)
+		fmt.Printf("%s: %.1f %.1f %.1f\n", key, measurement.Min, avg, measurement.Max)
+	}
 }
